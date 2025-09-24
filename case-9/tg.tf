0ff763996b5e76e99775bd656dcf8907ee353725
Aurimas <vecrofund@gmail.com>
2025-09-24 17:53:44 +0530

    Updated

--- case-9/tg.tf
+++ case-9/tg.tf
@@ -20,3 +20,13 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "case9-tg-vpc2-attachment" {
         Name = "case9-tg-vpc2-attachment"
     }
 }
+resource "aws_route" "case9-tg-vpc1-to-vpc2" {
+    route_table_id         = aws_route_table.case9-vpc1-public-rt.id
+    destination_cidr_block = aws_vpc.case9-vpc-2.cidr_block
+    transit_gateway_id     = aws_ec2_transit_gateway.case9-tg.id
+}
+resource "aws_route" "case9-tg-vpc2-to-vpc1" {
+    route_table_id         = aws_route_table.case9-vpc2-public-rt.id
+    destination_cidr_block = aws_vpc.case9-vpc-1.cidr_block
+    transit_gateway_id     = aws_ec2_transit_gateway.case9-tg.id
+}
